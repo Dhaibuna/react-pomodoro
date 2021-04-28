e165fae22808d37d52f2520530060814a5a30ae5
Dhaibuna <nataschagritte@gmail.com>
2021-04-28 16:12:51 +0200

    Update coding stucture

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+
+// Header of my App, inluding : title 
+
+const Header = () => {
+    return(
+        <div>
+            <h1>Pomodomon</h1>
+        </div>
+    )
+}
+export default Header 
