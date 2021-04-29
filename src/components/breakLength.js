a82bd347b244068b73b9b38ac8e9a1f20991c667
Dhaibuna <nataschagritte@gmail.com>
2021-04-29 22:09:53 +0200

    Update

--- src/components/breakLength.js
+++ src/components/breakLength.js
@@ -1,29 +1,30 @@
+import moment from "moment";
 import React, {useState} from "react";
 
 const SetBreakLength = () => {
     // start configuration Break Time Length configuration
-    const [count, setCount] = useState(5);
+    const [count, setCount] = useState(300);
 
     //Increase button
     const increaseCount =() =>{
-        setCount(count+1);
+        setCount(count+60);
     }
     //Decrease button
     const decreaseCount =()=>{
-
         if (count > 0) {
-            setCount(count - 1);
+            setCount(count-60);
         }else
             alert("Come on, take a break ;) !");
-            setCount(0);
     }
+
+    const BreakTimeInMinutes = moment.duration(count, 's').minutes()
 
     return(
         <div class="timelength-container">
             <h2> Break time length</h2>
             <div>
                 <button onClick={decreaseCount} >-</button>
-                <span>{count}</span>
+                <span>{BreakTimeInMinutes}</span>
                 <button onClick={increaseCount}>+</button>
             </div>
         </div>
